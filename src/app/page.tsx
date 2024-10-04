e6e07d386e695590a2ac75514ab5f2b91a02919b
면지 <hanguel97@gmail.com>
2024-10-04 13:28:36 +0900

    fix: build error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,5 @@
-import MovieArea from "@/components/movie-area";
-import MovieHeader from "@/components/movie-header";
-import MovieMain from "@/components/movie-main";
+import React from "react";
 
-export default function Home() {
-  return (
-    <>
-      <MovieHeader />
-      <MovieMain />
-      <MovieArea path="now_playing" title="Now Playing" />
-      <MovieArea path="popular" title="Popular" />
-      <MovieArea path="top_rated" title="Top Rated" />
-    </>
-  );
+export default function page() {
+  return <div>page</div>;
 }
