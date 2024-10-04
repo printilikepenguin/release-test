4b52a25c45fc3e603180a464c14382419bad968e
면지 <hanguel97@gmail.com>
2024-10-04 13:40:37 +0900

    add: page 컴포넌트 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
-import React from "react";
+import MovieArea from "@/components/movie-area";
+import MovieHeader from "@/components/movie-header";
+import MovieMain from "@/components/movie-main";
 
-export default function page() {
-  return <div>page</div>;
+export default function Home() {
+  return (
+    <>
+      <MovieHeader />
+      <MovieMain />
+      <MovieArea path="now_playing" title="Now Playing" />
+      <MovieArea path="popular" title="Popular" />
+      <MovieArea path="top_rated" title="Top Rated" />
+    </>
+  );
 }
